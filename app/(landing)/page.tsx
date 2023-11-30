19ca0ca9fc2ada8ac842572874d8d4d025903f58
sessionkiller <najib.doukkani@gmail.com>
2023-11-30 14:25:56 +0100

    Landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,15 @@
+import { LandingContent } from "@/components/landing-content";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
+
 const LandingPage = () => {
-    return(
-        <div>Landing Page (Unprotected)</div>
-    )
-}
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+  );
+};
 
 export default LandingPage;
